34b5bd73386af6ebb3f03f7abf73b1c659821f92
francienbarkhof <fj.barkhof@hotmail.com>
2023-01-10 11:14:59 +0100

    Update grover_algorithm.py

--- grover_algorithm.py
+++ grover_algorithm.py
@@ -35,7 +35,10 @@ if __name__ == '__main__':
     if 'authentication' not in vars().keys():
         authentication = get_authentication()
     QI.set_authentication_details(*authentication)
-    qi_backend = QI.get_backend('QX single-node simulator')
+
+    sim = Aer.get_backend('aer_simulator')
+
+    # qi_backend = QI.get_backend('QX single-node simulator')
 
     n = 2
     grover_circuit = QuantumCircuit(n)
@@ -49,7 +52,6 @@ if __name__ == '__main__':
     grover_circuit.z([0,1])
     grover_circuit.cz(0,1)
     grover_circuit.h([0,1])
-    grover_circuit.draw()
 
     print(grover_circuit)
 
@@ -57,3 +59,23 @@ if __name__ == '__main__':
     # q_result = q_obj.restult()
     # histogram = q_result.get_counts(grover_circuit)
     # plot_histogram(histogram)
+
+    # we need to make a copy of the circuit with the 'save_statevector'
+    # instruction to run on the Aer simulator
+    grover_circuit_sim = grover_circuit.copy()
+    grover_circuit_sim.save_statevector()
+    qobj = assemble(grover_circuit_sim)
+    result = sim.run(qobj).result()
+    statevec = result.get_statevector()
+
+    print(statevec)
+
+    grover_circuit.measure_all()
+
+    aer_sim = Aer.get_backend('aer_simulator')
+    qobj = assemble(grover_circuit)
+    result = aer_sim.run(qobj).result()
+    counts = result.get_counts()
+    print(counts)
+    # plot_histogram(counts)
+
